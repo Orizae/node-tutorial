0d83de7428df0dc574edc38945a1bc00c15b2f49
unknown <goncalocagica@gmail.com>
2024-03-27 18:17:45 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount = 8;
+
+if(amount < 10) {
+  console.log("This number is lower than 10.")
+}
+
+else{
+  console.log("This number bigger than 10.")
+}
+
+console.log(`My first Node app.`)
---
+++ 3-Modules.js
@@ -0,0 +1,14 @@
+// CommonJS, every file is a module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour');
+require('./7-mind-grenade');
+
+// console.log(names);
+
+// sayHi(names.peter);
+
+// sayHi(peter)
+// sayHi(john)
